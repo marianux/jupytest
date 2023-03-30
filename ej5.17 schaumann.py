92b6f76c12e0bf0cbf36422248d6766949f9484b
mariano <llamedom@gmail.com>
2023-03-30 16:34:18 -0300

    pequeños bugs en los ejemplos

--- ej5.17 schaumann.py
+++ ej5.17 schaumann.py
@@ -8,11 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 
+
 from pytc2.sistemas_lineales import analyze_sys, pretty_print_lti, tf2sos_analog, pretty_print_SOS
 
 
-num = np.array([1, 0, 2.5, 0, 0.5625, 0])
-den = np.array([1, 0.390, 3.067 , 0.785, 3.056, 0.387, 0.989])
+num = np.polymul([1, 0, 29.2**2], [1, 0, 43.2**2])
+den = np.polymul(np.polymul([1, 16.8], [1, 19.4, 20.01**2]), [1, 4.72, 22.52**2])
 
 pretty_print_lti(num, den)
 
@@ -27,7 +28,7 @@ _, mag, _ = thisFilter.bode(np.logspace(-1,1,1000))
 k0db = np.max(mag)
 
 # k0 deseada en el ejemplo en dB
-k0_deseada = 10 
+k0_deseada = 13.5
 
 # factor de corrección de ganancia
 k_corr = 10**((k0_deseada - k0db)/20)
