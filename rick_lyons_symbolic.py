73e88d1ba0d546707b396890c031856a9ec3872e
mariano <llamedom@gmail.com>
2023-11-22 15:47:18 -0300

    análisis simbólico del filtro RL funcionando

--- rick_lyons_symbolic.py
+++ rick_lyons_symbolic.py
@@ -63,10 +63,6 @@ def transf_s_2ba( T_s ):
     
     return( (bb,aa) )
 
-DD = 64
-UU = 20
-    
-bb2_16, aa2_16 = transf_s_2ba( Tdc_removal_2.subs({z:z**UU, D:DD}) )
 
 # display(Tdc_removal_2)
 
@@ -81,71 +77,225 @@ den = (sp.expand(den/(D**4*z**(4*D+4)))).powsimp()
 
 Tdc_removal_4 = num/den
 
-bb4_16, aa4_16 = transf_s_2ba( Tdc_removal_4.subs({z:z**UU, D:DD}) )
-
 # display(Tdc_removal_4)
 
 #%% Parte numérica 
 
-# Cargar el módulo Cythonizado
-from recursive_fir_filter import filter_sequence
-
-def one_MA_stage( double[:] xx, DD, UU):
-    
-    nn = len(vec)
-    cdef double[:] yy
-    cdef int i
-    for i in range(nn):
-        aux = aux + (1- aux)*vec[i]
-    return aux
-    
-    
-    return(yy)
-
-
-
-
 fs = 1000 # Hz (Normalizamos a fs/2 = f_nyq)
 nyq_frec = fs / 2
-
-
-# fpw = w0*np.pi*fs/np.tan(np.pi/2*w0); 
-
-## Rick Lyons ECG filter
-
-# demora_rl = int(uu*(dd-1))
-demora2_rl = (len(bb2_16)-1)/2
-demora2_rl = (len(bb4_16)-1)/2
-
 
 N = 4000
 w_rad  = np.append(np.logspace(-2, 0.8, N//4), np.logspace(0.9, 1.6, N//4) )
 w_rad  = np.append(w_rad, np.linspace(40, nyq_frec, N//2, endpoint=True) ) / nyq_frec * np.pi
 
-z,p,k = sig.tf2zpk(bb2_16, aa2_16)
 
-sos_rl2 = sig.tf2sos(bb2_16, aa2_16, pairing='nearest')
-sos_rl4 = sig.tf2sos(bb4_16, aa4_16, pairing='nearest')
+def group_delay( freq, phase):
+    
+    dphase = -np.diff(np.unwrap(phase, period = 19/10* np.pi ))
+    # dphase = -np.diff(phase)
+    # # corregir saltos de fase
+    # bAux = dphase > np.pi
+    # dphase[bAux] = np.amin( np.hstack([dphase[bAux], dphase[bAux]-np.pi]), axis = 1 )
+    groupDelay = dphase/np.diff(freq)
+    
+    return(np.append(groupDelay, groupDelay[-1]))
+    
 
-_, hh2_rl = sig.sosfreqz(sos_rl2, w_rad)
-_, hh4_rl = sig.sosfreqz(sos_rl4, w_rad)
 
-w = w_rad / np.pi * nyq_frec
+def Sym_freq_response(HH, zz, ww):
+    
+    # w = sp.Symbol('w', real=True)
+    # HH_jw = HH.subs({z:1*sp.exp_polar(sp.I*w)})
+    
+    H_numeric = sp.lambdify(zz, HH, modules=['numpy'])
+    
+    z_vals = np.exp(1j * ww)  # Rango de frecuencias angulares
+    
+    # Evalúa la función numérica en el rango de frecuencias angulares
+    magnitude_response = np.abs(H_numeric(z_vals))
+    phase_response = np.angle(H_numeric(z_vals))  # La fase se devuelve en grados
 
-plt.close('all')
+    return((magnitude_response, phase_response))
 
+
+def plt_freq_resp(title, magnitude_response, phase_response, w_rad, fs = 2):
+    
+    ww = w_rad / np.pi * fs/2
+
+    # Grafica la respuesta en frecuencia de módulo
+    plt.figure(figsize=(10, 5))
+    plt.subplot(2, 1, 1)
+    plt.plot(ww, 20 * np.log10(magnitude_response))
+    
+    plt.title('Respuesta en Frecuencia de Módulo: ' + title)
+    plt.xlabel('Frecuencia Angular (w)')
+    plt.ylabel('|H(jw)| (dB)')
+
+    plt.axis([0, 100, -60, 5 ]);
+    
+    gd = group_delay( w_rad, phase_response)
+    mgd = np.nanmedian(gd)
+    
+    # Grafica la respuesta en frecuencia de retardo
+    plt.subplot(2, 1, 2)
+    plt.plot(ww, gd)
+    plt.title('Respuesta de retardo')
+    plt.xlabel('Frecuencia Angular (w)')
+    plt.ylabel('Retardo (#)')
+    plt.axis([0, 100, mgd-5, mgd+5 ]);
+    
+    plt.tight_layout()
+    plt.show()
+    
+
+## Del análisis simbólico
+DD = [16, 32, 64]
+UU = 20
+  
+
+# Grafica la respuesta en frecuencia de módulo
 plt.figure(1)
-plt.clf()
 
-plt.plot(w, 20 * np.log10(abs(hh2_rl)), label='FIR-RL-2-D{:d} orden:{:d}'.format(2, DD))
-plt.plot(w, 20 * np.log10(abs(hh4_rl)), label='FIR-RL-2-D{:d} orden:{:d}'.format(4, DD))
-# plot_plantilla(filter_type = 'bandpass', fpass = frecs[[2, 3]]* nyq_frec, ripple = ripple , fstop = frecs[ [1, 4] ]* nyq_frec, attenuation = atenuacion, fs = fs)
+ww = w_rad / np.pi * fs / 2
 
-plt.title('FIR diseñado por métodos directos')
-plt.xlabel('Frequencia [Hz]')
-plt.ylabel('Modulo [dB]')
-plt.axis([0, 100, -60, 5 ]);
+for ddd in DD:
+    
+    # Cálculo de demoras para 2 mov. avg
+    demora_rl2 = int(UU*(ddd-1))
+
+    Tdcr_2 = Tdc_removal_2.subs({z:z**UU, D:ddd})
+
+    mod_Tdcr_2, pha_Tdcr_2 = Sym_freq_response(Tdcr_2, z, w_rad )
+
+    # plt_freq_resp('FIR-RL-2MA-D{:d}-OverS:{:d}'.format(DD, UU), mod_Tdcr_2, pha_Tdcr_2, w_rad, fs = fs)
+    plt.plot(ww, 20 * np.log10(mod_Tdcr_2), label = 'D:{:d} (#) - GD:{:3.1f} (#)'.format(ddd, demora_rl2) )
+
+plt.title('Respuesta en Frecuencia de Módulo: RL-2MA-OverS:{:d}'.format(UU))
+plt.xlabel('Frecuencia Angular (w)')
+plt.ylabel('|H(jw)| (dB)')
 plt.legend()
+plt.axis([0, 100, -1, 1 ]);
 
-plt.grid()
+plt.figure(2)
+
+for ddd in DD:
+
+    # Cálculo de demoras para 4 mov. avg
+    demora_rl4 = int(2*UU*(ddd-1))
+    
+    Tdcr_4 = Tdc_removal_4.subs({z:z**UU, D:ddd})
+    
+    mod_Tdcr_4, pha_Tdcr_4 = Sym_freq_response(Tdcr_4, z, w_rad )
+    
+    # plt_freq_resp('FIR-RL-4MA-D{:d}-OverS:{:d}'.format(DD, UU), mod_Tdcr_4, pha_Tdcr_4, w_rad, fs = fs)
+    
+    # plt.subplot(2, 1, 1)
+    plt.plot(ww, 20 * np.log10(mod_Tdcr_4), label = 'D:{:d} (#) - GD:{:3.1f} (#)'.format(ddd, demora_rl4) )
+
+plt.title('Respuesta en Frecuencia de Módulo: RL-4MA-OverS:{:d}'.format(UU))
+plt.xlabel('Frecuencia Angular (w)')
+plt.ylabel('|H(jw)| (dB)')
+plt.legend()
+plt.axis([0, 100, -1, 1 ]);
+
+plt.tight_layout()
+plt.show()
+
+
+# La respuesta de fase es tan grande que se dificulta calcular y visualizar.
+# ver variable demora_rl2
+# 
+# gd2 = group_delay( w_rad, pha_Tdcr_2)
+# gd4 = group_delay( w_rad, pha_Tdcr_4)
+# mgd2 = np.nanmedian(gd2)
+# mgd4 = np.nanmedian(gd4)
+
+# plt.figure(2)
+# # Grafica la respuesta en frecuencia de retardo
+# # plt.subplot(2, 1, 2)
+# plt.plot(ww, gd2, label = 'FIR-RL-2MA-D{:d}-OverS:{:d}'.format(DD, UU))
+# plt.plot(ww, gd4, label = 'FIR-RL-4MA-D{:d}-OverS:{:d}'.format(DD, UU))
+
+# plt.title('Respuesta de retardo')
+# plt.xlabel('Frecuencia Angular (w)')
+# plt.ylabel('Retardo (#)')
+# plt.axis([0, 100, np.min([mgd2,mgd4])-5, np.max([mgd2,mgd4])+5 ]);
+
+# plt.tight_layout()
+# plt.show()
+
+
+# coeficientes
+# bb2, aa2 = transf_s_2ba( Tdcr_2)
+
+# bb4, aa4 = transf_s_2ba( Tdcr_4)
+
+
+# ##  Implementación via Cython
+# from recursive_fir_filter import filter_sequence
+
+
+# ## Implementación Python
+# def one_MA_stage( xx, DD, UU):
+    
+#     NN = xx.shape[0]
+#     buffer_size = DD * UU
+#     yy = np.zeros_like(xx)
+
+#     for kk in range(NN):
+
+#         # Calcula la salida según la ecuación recursiva
+#         yy[kk] = 1.0 / (DD * UU) * (xx[kk] - xx[ (kk - DD * UU) % buffer_size] + yy[(kk - UU)% buffer_size])
+
+#     return(yy)
+
+
+# def Tdc_removal( xx, DD = 16, UU = 20, MA_stages = 2 ):
+    
+#     yy = one_MA_stage( xx, DD, UU)
+
+#     # cascadeamos MA_stages-1 más
+#     for ii in range(1, MA_stages):
+#         yy = one_MA_stage( yy, DD, UU)
+    
+#     return(yy - np.roll(xx, (DD-1)/2*MA_stages*UU))
+
+
+
+# # fpw = w0*np.pi*fs/np.tan(np.pi/2*w0); 
+
+# ## Rick Lyons ECG filter
+
+# # demora_rl = int(uu*(dd-1))
+# demora2_rl = (len(bb2)-1)/2
+# demora2_rl = (len(bb4)-1)/2
+
+
+
+# z,p,k = sig.tf2zpk(bb2, aa2)
+
+# sos_rl2 = sig.tf2sos(bb2, aa2, pairing='nearest')
+# sos_rl4 = sig.tf2sos(bb4, aa4, pairing='nearest')
+
+# _, hh2_rl = sig.sosfreqz(sos_rl2, w_rad)
+# _, hh4_rl = sig.sosfreqz(sos_rl4, w_rad)
+
+# w = w_rad / np.pi * nyq_frec
+
+# plt.close('all')
+
+# plt.figure(1)
+# plt.clf()
+
+# plt.plot(w, 20 * np.log10(abs(hh2_rl)), label='FIR-RL-2-D{:d} orden:{:d}'.format(2, DD))
+# plt.plot(w, 20 * np.log10(abs(hh4_rl)), label='FIR-RL-2-D{:d} orden:{:d}'.format(4, DD))
+# # plot_plantilla(filter_type = 'bandpass', fpass = frecs[[2, 3]]* nyq_frec, ripple = ripple , fstop = frecs[ [1, 4] ]* nyq_frec, attenuation = atenuacion, fs = fs)
+
+# plt.title('FIR diseñado por métodos directos')
+# plt.xlabel('Frequencia [Hz]')
+# plt.ylabel('Modulo [dB]')
+# plt.axis([0, 100, -60, 5 ]);
+# plt.legend()
+
+# plt.grid()
 
