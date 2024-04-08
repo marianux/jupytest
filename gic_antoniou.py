a16ce04e40d6880c9f26aee7f0ab20f343f9bfb8
marianux <llamedom@gmail.com>
2024-04-08 06:43:17 -0300

    pruebas en GIC

--- gic_antoniou.py
+++ gic_antoniou.py
@@ -7,15 +7,17 @@ Created on Mon Apr 11 10:52:56 2022
 """
 
 import sympy as sp
-from sympy.abc import s
-from IPython.display import display, Math
+from scipy.signal import TransferFunction
+
 
 from pytc2.sintesis_dipolo import foster
-from pytc2.remociones import remover_polo_dc, remover_polo_infinito
+from pytc2.remociones import remover_polo_dc, remover_polo_infinito, isFRP
+from pytc2.general import a_equal_b_latex_s, print_latex, s
+from pytc2.sistemas_lineales import analyze_sys
 
 I1, V1, V2, V3, V4, V5 = sp.symbols("I1, V1, V2, V3, V4, V5")
-Y1, Y2, Y3, Y4, Y5, As, wt, w0 = sp.symbols("Y1, Y2, Y3, Y4, Y5, As, wt, w0")
-G, C = sp.symbols("G, C") 
+Y1, Y2, Y3, Y4, Y5, As = sp.symbols("Y1, Y2, Y3, Y4, Y5, As")
+G, C, wt, w0 = sp.symbols("G, C, wt, w0", real = True) 
 
 # modelo ideal negativamente realimentado
 aa = sp.solve([ 
@@ -30,8 +32,8 @@ print('##############################')
 print('# Z1 para el GIC de Antoniou #')
 print('##############################')
 
-display(Math( r' Z_1^i = ' + sp.latex(Z1) ))
-
+# display(Math( r' Z_1^i = ' + sp.latex(Z1) ))
+print_latex(a_equal_b_latex_s('Z_1^i', Z1))
 
 ######################################################
 # solo modelo ideal, no sabemos qué realimentación es. 
@@ -53,7 +55,8 @@ print('#############################################################')
 print('# Z1 para el GIC de Antoniou sin considerar realim negativa #')
 print('#############################################################')
 
-display(Math( r' Z_1^{ir} = ' + sp.latex(Z1_opamp_ideal) ))
+# display(Math( r' Z_1^{ir} = ' + sp.latex(Z1_opamp_ideal) ))
+print_latex(a_equal_b_latex_s('Z_1^{ir}', Z1_opamp_ideal))
 
 
 # modelo integrador A(s)=\omega_t/s (sin asumir realimentación negativa)
@@ -78,25 +81,50 @@ Z1_opamp_real  = num/den*k
 print('#################################################')
 print('# Z1 para el GIC de Antoniou (OpAmp integrador) #')
 print('#################################################')
-display(Math( r' Z_1^r = ' + sp.latex(Z1_opamp_real) ))
+# display(Math( r' Z_1^r = ' + sp.latex(Z1_opamp_real) ))
+print_latex(a_equal_b_latex_s('Z_1^r', Z1_opamp_real))
 
 print('¿Qué tipo de Z1 será?')
 
+ww = sp.symbols("\omega", real = True)
+
 # Si G/C = w0 = 1; wt = 1000 y G = 1
-z1_real = (s**3 + s**2 * 1001 + s * (10**6/2+10**3) ) / (s**3 + s**2 * 1001 + s * 10**3 + 10**6/2 )
+z1_realopamp = sp.simplify(sp.expand(Z1_opamp_real.subs({wt:100, G:1, C:1})))
 
-Ginf = sp.limit(1/z1_real, s, sp.oo)
-G0 = sp.limit(1/z1_real, s, 0)
+num, den = sp.fraction(z1_realopamp)
 
-R1 = 1/Ginf
+num.as_poly(s).all_coeffs()
+    
+aa = np.array( [ float(ii) for ii in num.as_poly(s).all_coeffs()]
+bb = np.array( [ float(ii) for ii in den.as_poly(s).all_coeffs()]
+
+cc = TransferFunction(aa, bb)
+                      
+analyze_sys(  )
+
+
+# re_z1_realopamp = sp.simplify(sp.expand(sp.re(z1_realopamp.subs({s:(sp.I*ww)}))))
+# re_y1_realopamp = sp.simplify(sp.expand(sp.re(1/z1_realopamp.subs({s:(sp.I*ww)}))))
+re_z1_realopamp = sp.simplify(sp.expand(sp.re(Z1_opamp_real.subs({s:(sp.I*ww)}))))
+
+
+Goo = sp.limit(1/Z1_opamp_real, s, sp.oo)
+G0 = sp.limit(1/Z1_opamp_real, s, 0)
+
+Y2 = sp.factor(sp.simplify(sp.expand(1/Z1_opamp_real - Goo)))
+
+
+Goo = sp.limit(1/z1_realopamp, s, sp.oo)
+G0 = sp.limit(1/z1_realopamp, s, 0)
+
 
 # remuevo la menor admitancia
-Y2 = sp.factor(sp.simplify(sp.expand(1/z1_real - Ginf)))
+Y2 = sp.factor(sp.simplify(sp.expand(1/z1_realopamp - Goo)))
 
-Y4, Yl1 = remover_polo_dc(Y2)
+# Y4, Yl1 = remover_polo_dc(Y2)
 
 # Zl1  es la impedancia removida
 # extraigo L1
-L1 = Yl1/s
+# L1 = Yl1/s
 
 
